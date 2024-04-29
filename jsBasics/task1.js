8c19bb6c457787968439a4437534dc19dfd5165c
Alla Varavina <fistawka27@gmail.com>
2024-04-29 23:16:38 +0200

    homeWork6

---
+++ jsBasics/task1.js
@@ -0,0 +1,14 @@
+//Створіть програму, в якій ви оголосите змінні для кожного з примітивних типів даних:
+//рядок, число, буль, null та undefined.
+//Присвойте їм значення та виведіть кожну змінну на консоль.
+
+let string = "Hello!";
+let number = 36;
+let data_bollean = true;
+let data_null = null;
+let data_undefined = undefined;
+console.log(string);
+console.log(number);
+console.log(data_bollean);
+console.log(data_null);
+console.log(data_undefined);
