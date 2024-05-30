da52ddb2e8e43bba2095f3a30fb6ef2dd90913b2
Alla Varavina <fistawka27@gmail.com>
2024-05-30 23:25:40 +0200

    homeWork11

---
+++ homeWork_11/task2.2.js
@@ -0,0 +1,30 @@
+const fetchResidentsAllPlanet = async () => {
+  try {
+    const filmResponse = await fetch("https://swapi.dev/api/films/3/");
+    const film = await filmResponse.json();
+
+    const planetPromises = film.planets.map((url) => fetch(url));
+    const planetResponses = await Promise.all(planetPromises);
+    const planets = await Promise.all(
+      planetResponses.map((response) => response.json())
+    );
+
+    const planetResidents = await Promise.all(
+      planets.map(async (planet) => {
+        const residentPromises = planet.residents.map((url) => fetch(url));
+        const residentResponses = await Promise.all(residentPromises);
+        const residents = await Promise.all(
+          residentResponses.map((response) => response.json())
+        );
+        const residentNames = residents.map((resident) => resident.name);
+        return { planetName: planet.name, residents: residentNames };
+      })
+    );
+
+    console.log(planetResidents);
+  } catch (error) {
+    console.error("Error fetching residents:", error);
+  }
+};
+
+fetchResidentsAllPlanet();
